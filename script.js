4aa5903878ed848cd31970941df41c6d9dccb75c
atran005 <atran005@stu.waukesha.k12.wi.us>
2026-05-14 14:43:46 -0500

    deploy script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 var appointment = {};
-let obj = [{ guy: "Bob", bro: "Bob" }, { guy: "Jon", bro: "Jon" }, { guy: "Tom", bro: "Tom" }];
-let randomGuy = obj.find(person => person.guy.startsWith("J"));
-alert(JSON.stringify(randomGuy.guy));
 const button = index.html.getElementById("test_Button");
  function handleButtonClicks() {
       console.log(click);
