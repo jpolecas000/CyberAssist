9badbea91efe5260e8b990b98c9e125b857dca97
loz_fan2012 <jpolecas000@stu.waukesha.k12.wi.us>
2026-05-14 09:32:59 -0500

    modify script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 var appointment = {};
 let obj = [{ guy: "Bob", bro: "Bob" }, { guy: "Jon", bro: "Jon" }, { guy: "Tom", bro: "Tom" }];
 let randomGuy = obj.find(person => person.guy.startsWith("J"));
-alert(JSON.stringify(obj.randomGuy));
+alert(JSON.stringify(randomGuy.guy));
